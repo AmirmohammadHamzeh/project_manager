346e5767def63c780171b099d32b9506d904528b
tandorost <amrimohammadhamzehi@gmail.com>
2025-07-08 10:23:24 +0330

    add task end points

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import ProjectListCreateView, ProjectDetailView, AddMemberView
+from .views import TaskListCreateView, TaskDetailView, ProjectTaskListView
+
 
 urlpatterns = [
     path('projects/', ProjectListCreateView.as_view(), name='project_list_create'),
     path('projects/<int:project_id>/', ProjectDetailView.as_view(), name='project_detail'),
     path('projects/<int:project_id>/add-member/', AddMemberView.as_view(), name='add_project_member'),
+    path('tasks/', TaskListCreateView.as_view(), name='task_list_create'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    path('projects/<int:project_id>/tasks/', ProjectTaskListView.as_view(), name='project_tasks'),
 ]
